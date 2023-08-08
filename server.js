dc6da2a2ba17f7b90aa9bb9d38d632414f4ac0b1
Amr Soliman <thisissoliman@protonmail.com>
2023-08-08 16:39:26 +0300

    testing-server-cronJob

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
+import express from "express";
+
 const app = express();
-app.get('/', (req, res) => {
-  res.status(200).json({ status: 'running' });
+app.get('/', (_, res) => {
+  res.status(200).send("<h1>THIS IS A DEMO</h1>");
 });
 app.listen(3000);
 console.log('app is listening on 3000');
