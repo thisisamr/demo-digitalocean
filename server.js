29d09ed8daee0d95dbdf1a750d8ec177774244e6
Amr Soliman <thisissoliman@protonmail.com>
2023-08-08 16:57:31 +0300

    testeing-bash-script-pm2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 app.get('/', (_, res) => {
-  res.status(200).send("<h1>THIS IS A DEMO</h1>");
+  res.status(200).send("<body style='color:black;'><h1 style ='color:white;'>THIS IS A DEMO</h1><body>");
 });
 app.listen(3000);
 console.log('app is listening on 3000');
